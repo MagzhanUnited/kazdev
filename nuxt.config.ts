53d08d4a141a47494b1e48f8456e9ffa22ae98dd
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2024-02-10 02:25:39 +0600

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
   app: {
     baseURL: "/kazdev/", // baseURL: '/<repository>/'
     buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
