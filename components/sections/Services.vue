6753f660754588c623f54d56182429c7269f6557
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2024-02-10 02:00:51 +0600

    init

---
+++ components/sections/Services.vue
@@ -0,0 +1,177 @@
+<template>
+  <section py-20>
+    <div
+      px="5 sm:10 md:12 lg:5"
+      class="max-w-7xl mx-auto flex flex-col items-start gap-10 xl-gap-14"
+    >
+      <div class="text-center max-w-3xl mx-auto space-y-4">
+        <h1 text="4xl neutral-9 dark:white" class="font-semibold">
+          Что мы предлагаем
+        </h1>
+        <p text="neutral-7 dark:neutral-3">
+          Мы предоставляем разработчиков и дизайнеров, использующих новейшие
+          технологии, чтобы помочь вам расширить свой бизнес
+        </p>
+      </div>
+      <div class="grid sm-grid-cols-2 lg-grid-cols-3 gap-6 lg-gap-8">
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-tag-edit flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Разработка программного обеспечения
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Индивидуальные программные решения для удовлетворения конкретных
+              потребностей бизнеса.
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-email flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Управление ИТ-инфраструктурой
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Комплексное управление ИТ-инфраструктурой, включая сети, серверы и
+              системы
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-logo-glassdoor flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Решения для обеспечения кибербезопасности
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Защита бизнеса от киберугроз с помощью надежных мер и стратегий
+              безопасности
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-tag-edit flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Служба поддержки 24/7
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Круглосуточная техническая поддержка обеспечивает бесперебойную
+              работу и оперативное решение любых проблем
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-tag-edit flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Услуги по анализу данных
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Использование данных для получения аналитической информации и
+              принятия обоснованных решений предприятиями
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+        <div
+          p="5 sm:6 lg:8"
+          border="~ neutral-2 dark:neutral-8"
+          bg="neutral-1 dark:neutral-9"
+          class="rd-3xl relative overflow-hidden"
+        >
+          <div
+            bg="neutral-2 dark:neutral-8"
+            text="neutral-9 dark:white"
+            class="rd-xl p3 w-max relative"
+          >
+            <span i-carbon-tag-edit flex text-2xl></span>
+          </div>
+          <div class="mt6 space-y-4 relative">
+            <h2 text="lg md:xl neutral-9 dark:neutral-1" class="font-semibold">
+              Миграция государственных систем
+            </h2>
+            <p text="neutral-7 dark:neutral-3">
+              Опыт в миграции и интеграции ИТ-систем с государственными
+              платформами, обеспечение соответствия требованиям и бесперебойной
+              работы
+            </p>
+          </div>
+          <span
+            class="absolute w32 aspect-square -bottom-16 -right-16 bg-emerald-6/10 rd-full"
+          ></span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
