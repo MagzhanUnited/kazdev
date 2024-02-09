2bd302a1087a376bdb10ecb4e447338b94b36938
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2024-02-10 02:17:15 +0600

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules:[
-    '@unocss/nuxt',
-    '@nuxtjs/color-mode',
-    '@nuxt/image'
-  ],
-  colorMode: {
-    preference: 'system', 
-    fallback: 'dark', 
-    classPrefix: '',
-    classSuffix: '',
-    storageKey: 'app-theme'
+  app: {
+    baseURL: "/nuxt-github-pages/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
-})
+  modules: ["@unocss/nuxt", "@nuxtjs/color-mode", "@nuxt/image"],
+  colorMode: {
+    preference: "system",
+    fallback: "dark",
+    classPrefix: "",
+    classSuffix: "",
+    storageKey: "app-theme",
+  },
+});
